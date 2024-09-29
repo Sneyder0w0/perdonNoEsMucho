e5b6bb691554be2b950a103c70e7898cf4b74561
Sneyder Orellana <forsakenpenumbra@gmail.com>
2024-09-29 00:13:26 -0600

    perdoooon....

---
+++ main.js
@@ -0,0 +1,13 @@
+var camisas = document.getElementById("camisas");
+      if (camisas) {
+        camisas.addEventListener("click", function (e) {
+          window.location.href = "html/camisas.html";
+        });
+      }
+
+var pantalones = document.getElementById("pantalones");
+      if (pantalones) {
+        pantalones.addEventListener("click", function (e) {
+          window.location.href = "html/pantalones.html";
+        });
+      }
